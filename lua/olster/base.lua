628527a88de06da9a64dee7be48861a16ff28e3a
Olivers11 <olstertecn597@outlook.com>
2023-03-07 09:36:16 -0600

    base config modify

--- lua/olster/base.lua
+++ lua/olster/base.lua
@@ -37,15 +37,6 @@ vim.opt.wildignore:append { '*/node_modules/*' }
 vim.cmd([[let &t_Cs = "\e[4:3m"]])
 vim.cmd([[let &t_Ce = "\e[4:0m"]])
 
--- Turn off paste mode when leaving insert
-vim.api.nvim_create_autocmd("InsertLeave", {
-  pattern = '*',
-  command = "set nopaste"
-})
-
--- Add asterisks in block comments
-vim.opt.formatoptions:append { 'r' }
-
 
 vim.opt.cursorline = true
 vim.opt.termguicolors = true
