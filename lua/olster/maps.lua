b1c6035581c58cd2e95119909534d0e398db5449
Olivers11 <olstertecn597@outlook.com>
2023-03-07 08:06:54 -0600

    adding commentary and telescope plugins

--- lua/olster/maps.lua
+++ lua/olster/maps.lua
@@ -9,6 +9,13 @@ end
 
 vim.g.mapleader = ' '
 
-
+-- Utils
 map('n', '<leader>so', ':source %<CR>')
+map('n', '<leader>t', ':t . <CR>')
 map('i', 'kk', '<Esc>')
+
+
+-- Telescope
+map('n', '<leader>ff', ':Telescope find_files<CR>')
+map('n', '<leader>fs', ':Telescope live_grep<CR>')
+map('n', '<leader>gc', ':Telescope git_commits<CR>')
--- lua/olster/plugins.lua
+++ lua/olster/plugins.lua
@@ -10,9 +10,28 @@ packer.startup(function(use)
   --Packer
   use('wbthomason/packer.nvim')
 
+  -- Lualine
   use {
     'nvim-lualine/lualine.nvim',
     requires = { 'kyazdani42/nvim-web-devicons', opt = true }
   }
+
+
+  -- Telescope
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.1',
+    -- or                            , branch = '0.1.x',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+
+  -- Commentary
+  use {
+      'numToStr/Comment.nvim',
+      config = function()
+          require('Comment').setup()
+      end
+  }
+
 end)
 
