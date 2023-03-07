26c12b4f6723784dbe0aa7aa9c8560662a8123d4
Olivers11 <olstertecn597@outlook.com>
2023-03-07 10:24:49 -0600

    change position of babe message

--- after/plugin/lualine.rc.lua
+++ after/plugin/lualine.rc.lua
@@ -17,12 +17,12 @@ lualine.setup {
     lualine_a = { 'mode' },
     lualine_b = { 'branch'},
     lualine_c = { {
-      babe,
       'filename',
       file_status = true, -- displays file status (readonly status, modified status)
       path = 0 -- 0 = just filename, 1 = relative path, 2 = absolute path
     } },
     lualine_x = {
+      babe,
       { 'diagnostics', sources = { "nvim_diagnostic" }, symbols = { error = ' ', warn = ' ', info = ' ',
         hint = ' ' } },
       'encoding',
