18bfe7fa13c80befcb166b238d95c8f99d5cc2e9
Olivers11 <olstertecn597@outlook.com>
2023-03-10 17:10:01 -0600

    removing clangd

--- after/plugin/lspconfig.rc.lua
+++ after/plugin/lspconfig.rc.lua
@@ -1,33 +1,33 @@
 -- Mappings.
-local opts = { noremap=true, silent=true }
+local opts = { noremap = true, silent = true }
 
 
 local on_attach = function(client, bufnr)
   vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-  local bufopts = { noremap=true, silent=true, buffer=bufnr }
+  local bufopts = { noremap = true, silent = true, buffer = bufnr }
 end
 
 local lsp_flags = {
   debounce_text_changes = 150,
 }
 
-require('lspconfig')['pyright'].setup{
-    on_attach = on_attach,
-    flags = lsp_flags,
+require('lspconfig')['pyright'].setup {
+  on_attach = on_attach,
+  flags = lsp_flags,
 }
-require('lspconfig')['tsserver'].setup{
-    on_attach = on_attach,
-    flags = lsp_flags,
-}
-
-require('lspconfig')['lua_ls'].setup{
-    on_attach = on_attach,
-    flags = lsp_flags,
+require('lspconfig')['tsserver'].setup {
+  on_attach = on_attach,
+  flags = lsp_flags,
 }
 
+require('lspconfig')['lua_ls'].setup {
+  on_attach = on_attach,
+  flags = lsp_flags,
+}
 
-require('lspconfig')['ccls'].setup{
-    on_attach = on_attach,
-    flags = lsp_flags,
+
+require('lspconfig')['ccls'].setup {
+  on_attach = on_attach,
+  flags = lsp_flags,
 }
