f8ffa87e9b64f419b390b15db80eef26a0707ad2
Olivers11 <olstertecn597@outlook.com>
2023-03-10 15:02:04 -0600

    adding coc in plugins

--- lua/olster/plugins.lua
+++ lua/olster/plugins.lua
@@ -9,6 +9,8 @@ vim.cmd [[packadd packer.nvim]]
 packer.startup(function(use)
   use 'nvim-lualine/lualine.nvim'
   use 'kyazdani42/nvim-web-devicons' 
+
+  -- Telescope
   use {
     'nvim-telescope/telescope.nvim', tag = '0.1.1',
     requires = { {'nvim-lua/plenary.nvim'} }
@@ -27,6 +29,9 @@ packer.startup(function(use)
         require('Comment').setup()
     end
   }
+
+  use {'neoclide/coc.nvim', branch = 'release'}
+
 
 
 
