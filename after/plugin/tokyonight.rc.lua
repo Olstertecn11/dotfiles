68b78923cb633242b1da27b2d2871574f943b94d
Olivers11 <olstertecn597@outlook.com>
2023-03-09 19:28:29 -0600

    fixing a problem

--- after/plugin/tokyonight.rc.lua
+++ after/plugin/tokyonight.rc.lua
@@ -1,4 +1,4 @@
-eequire("tokyonight").setup({
+require("tokyonight").setup({
   style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
   light_style = "day", -- The theme is used when the background is set to light
   transparent = true, -- Enable this to disable setting the background color
