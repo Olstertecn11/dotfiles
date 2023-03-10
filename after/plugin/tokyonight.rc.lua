625675e4d108eb3059e80f46801110620bb81f61
Olivers11 <olstertecn597@outlook.com>
2023-03-09 18:55:56 -0600

    quit comments of tokyo

--- after/plugin/tokyonight.rc.lua
+++ after/plugin/tokyonight.rc.lua
@@ -6,13 +6,10 @@ require("tokyonight").setup({
   transparent = true, -- Enable this to disable setting the background color
   terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
   styles = {
-    -- Style to be applied to different syntax groups
-    -- Value is any valid attr-list value for `:help nvim_set_hl`
     comments = { italic = true },
     keywords = { italic = true },
     functions = {},
     variables = {},
-    -- Background styles. Can be "dark", "transparent" or "normal"
     sidebars = "dark", -- style for sidebars, see below
     floats = "dark", -- style for floating windows
   },
@@ -22,14 +19,7 @@ require("tokyonight").setup({
   dim_inactive = false, -- dims inactive windows
   lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
 
-  --- You can override specific color groups to use other groups or a hex color
-  --- function will be called with a ColorScheme table
-  ---@param colors ColorScheme
   on_colors = function(colors) end,
 
-  --- You can override specific highlights to use other groups or a hex color
-  --- function will be called with a Highlights and ColorScheme table
-  ---@param highlights Highlights
-  ---@param colors ColorScheme
   on_highlights = function(highlights, colors) end,
 })
