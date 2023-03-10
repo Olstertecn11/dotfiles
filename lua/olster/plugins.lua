10925a238a4f2b4c24cd546529e9d00178c2bf1f
Olivers11 <olstertecn597@outlook.com>
2023-03-09 22:55:32 -0600

    adding telescope

--- lua/olster/plugins.lua
+++ lua/olster/plugins.lua
@@ -9,5 +9,8 @@ vim.cmd [[packadd packer.nvim]]
 packer.startup(function(use)
   use 'nvim-lualine/lualine.nvim'
   use 'kyazdani42/nvim-web-devicons' 
-  use 'nvim-telescope/telescope.nvim'
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.1',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  } 
 end)
