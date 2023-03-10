d2084157c9f9d4a1576dfe45fc523b31a6ffb624
Olivers11 <olstertecn597@outlook.com>
2023-03-09 18:22:38 -0600

    adding windows config in init.lua

--- init.lua
+++ init.lua
@@ -2,6 +2,15 @@ require('olster.base')
 require('olster.plugins')
 require('olster.maps')
 
+local has = vim.fn.has
+
+local is_win = has "win32"
+
+
+if is_win then 
+  require('olster.windows')
+end
+
 
 
 
