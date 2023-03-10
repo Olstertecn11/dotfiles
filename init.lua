d0d5dc423ed62b9ccc3848880fbdc2b33db2d98c
Olivers11 <olstertecn597@outlook.com>
2023-03-10 15:21:44 -0600

    moving things

--- init.lua
+++ init.lua
@@ -2,14 +2,17 @@ require('olster.base')
 require('olster.plugins')
 require('olster.maps')
 
+
 local has = vim.fn.has
+
 
 local is_win = has "win32"
 
 
-if is_win then 
+if is_win then
   require('olster.windows')
 end
+
 
 
 
