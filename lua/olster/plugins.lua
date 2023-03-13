a8d35bb0fc1f659f09ee4eb0956d1a55ec21b0f8
Olivers11 <olstertecn597@outlook.com>
2023-03-12 22:51:07 -0600

    adding lspsaga

--- lua/olster/plugins.lua
+++ lua/olster/plugins.lua
@@ -67,4 +67,17 @@ packer.startup(function(use)
     },
   })
 
+  use({
+    "glepnir/lspsaga.nvim",
+    branch = "main",
+    config = function()
+      require("lspsaga").setup({})
+    end,
+    requires = {
+      { "nvim-tree/nvim-web-devicons" },
+      --Please make sure you install markdown and markdown_inline parser
+      { "nvim-treesitter/nvim-treesitter" }
+    }
+  })
+
 end)
